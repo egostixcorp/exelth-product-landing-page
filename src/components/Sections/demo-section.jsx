6f83ca60805e843ef0361de59c427c685d88ceca
egostixcorp <initcompany0@gmail.com>
2025-05-28 10:50:40 +0530

    update

--- src/components/Sections/demo-section.jsx
+++ src/components/Sections/demo-section.jsx
@@ -4,7 +4,7 @@ import LaptopMockUp from "../Mock/laptop-mockup";
 const DemoSection = () => {
   return (
     <div className="redd flex flex-col gap-5 laptop:h-screen w-full items-center justify-start">
-      <h1 className="laptop:text-3xl font-semibold text-center w-full">Mordern Hospital Management System</h1>
+      <h1 className="laptop:text-3xl font-semibold text-center w-full">Mordern Health Care Management System</h1>
       <DesktopMockUp />
       {/* <LaptopMockUp screenImage="/mock/light-b2b-app.png" /> */}
     </div>
