74fbc6cc93518d519623fec972ff50ee74b97b92
egostixcorp <initcompany0@gmail.com>
2025-05-28 11:39:50 +0530

    update metadataBase Url

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://exelth.com/"),
   title: "Exelth | Centralized Digital Health Platform",
   description: "Build by Egostix",
 };
