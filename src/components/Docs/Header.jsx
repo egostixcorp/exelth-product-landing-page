4f1ffc1dafee69e44f93161bccf062dbfea80c1a
egostixcorp <initcompany0@gmail.com>
2025-08-11 13:04:17 +0530

    update support link

--- src/components/Docs/Header.jsx
+++ src/components/Docs/Header.jsx
@@ -26,7 +26,7 @@ const DocsHeader = () => {
 
       {/* Support Link */}
       <Link
-        href="/support"
+        href="/contact"
         className="rounded-lg bg-green-600 px-4 py-2 text-sm font-medium text-white hover:bg-green-700"
       >
         Contact Support
