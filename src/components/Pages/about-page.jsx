42a3b2bb9135c477922a61328cc18c8c5e67de1c
egostixcorp <initcompany0@gmail.com>
2025-09-22 13:40:54 +0530

    update link

--- src/components/Pages/about-page.jsx
+++ src/components/Pages/about-page.jsx
@@ -341,7 +341,7 @@ const AboutPage = () => {
             welcome.
           </p>
           <Link
-            href="/careers"
+            href=""
             className="mt-4 inline-block rounded-full bg-green-600 px-6 py-2 text-sm font-medium text-white hover:bg-green-700"
           >
             Explore Careers
