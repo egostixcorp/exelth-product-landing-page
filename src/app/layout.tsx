314777fa3aed27c2fd23148d302bafbb3630e35a
egostixcorp <initcompany0@gmail.com>
2025-10-17 12:36:42 +0530

    update meta pixel code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,8 @@ import { Inter } from "next/font/google";
 import dynamic from "next/dynamic";
 const FacebookPixel = dynamic(import("../lib/facebook-meta-pixel"));
 import "./globals.css";
+import Script from "next/script";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -50,6 +52,45 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          id="fb-pixel-base"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s){
+                if(f.fbq) return;
+                n = f.fbq = function(){
+                  n.callMethod
+                    ? n.callMethod.apply(n, arguments)
+                    : n.queue.push(arguments)
+                };
+                if(!f._fbq) f._fbq = n;
+                n.push = n;
+                n.loaded = true;
+                n.version = '2.0';
+                n.queue = [];
+                t = b.createElement(e);
+                t.async = true;
+                t.src = 'https://connect.facebook.net/en_US/fbevents.js';
+                s = b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t, s);
+              }(window, document, 'script');
+              fbq('init', '${process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID}');
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <Image
+            height={1}
+            width={1}
+            style={{ display: "none" }}
+            src={`https://www.facebook.com/tr?id=${process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID}&ev=PageView&noscript=1`}
+            alt=""
+          />
+        </noscript>
+      </head>
       <body className={inter.className}>
         <FacebookPixel />
         <LanguageProvider>
--- src/lib/facebook-meta-pixel.jsx
+++ src/lib/facebook-meta-pixel.jsx
@@ -1,39 +1,18 @@
-import Image from "next/image";
-import Script from "next/script";
-import React from "react";
+"use client";
 
-const FacebookPixel = () => {
-  return (
-    <>
-      <Script
-        strategy="afterInteractive"
-        id="facebook-pixel"
-        dangerouslySetInnerHTML={{
-          __html: `
-    !function(f,b,e,v,n,t,s)
-{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-n.queue=[];t=b.createElement(e);t.async=!0;
-t.src=v;s=b.getElementsByTagName(e)[0];
-s.parentNode.insertBefore(t,s)}(window, document,'script',
-'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', ${process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID});
-fbq('track', 'PageView');
-  `,
-        }}
-      />
-      <noscript>
-        <Image
-          height={1}
-          width={1}
-          style={{ display: "none" }}
-          alt="facebook-pixel"
-          src={`https://www.facebook.com/tr?id=${process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID}&ev=PageView&noscript=1`}
-        />
-      </noscript>
-    </>
-  );
-};
+import { useEffect } from "react";
+import { usePathname, useSearchParams } from "next/navigation";
 
-export default FacebookPixel;
+export default function PixelTracker() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    // This runs on mount and on any path / query change
+    if (window.fbq) {
+      window.fbq("track", "PageView");
+    }
+  }, [pathname, searchParams]);
+
+  return null;
+}
