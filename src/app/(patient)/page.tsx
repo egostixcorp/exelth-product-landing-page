eb9ab2a0cbfada4c79a684a3d2a8b547c4c289f4
egostixcorp <initcompany0@gmail.com>
2025-10-25 13:36:29 +0530

    update

---
+++ src/app/(patient)/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SearchPage from "@/components/App/Pages/SearchPages";
+const SearchPageScreen = () => {
+  return (
+    <div className="flex min-h-screen w-full items-center justify-center">
+      {/* Search Page */}
+      <SearchPage />
+    </div>
+  );
+};
+
+export default SearchPageScreen;
