395081269b95fe51c20014024180f4d7f3df9362
egostixcorp <initcompany0@gmail.com>
2025-10-25 13:43:19 +0530

    update re

---
+++ src/app/(marketing)/page.tsx
@@ -0,0 +1,35 @@
+// import React from "react";
+// import SearchPage from "@/components/App/Pages/SearchPages";
+// const SearchPageScreen = () => {
+//   return (
+//     <div className="flex min-h-screen w-full items-center justify-center">
+//       {/* Search Page */}
+//       <SearchPage />
+//     </div>
+//   );
+// };
+
+// export default SearchPageScreen;
+// "use client";
+import React, { useEffect } from "react";
+import HeroSection from "@/components/Sections/hero-section";
+import DemoSection from "@/components/Sections/demo-section";
+import GridLayout from "@/components/Layouts/grid-layout";
+import MobileDemoSection from "@/components/Sections/mobile-demo-section";
+const Home = () => {
+  return (
+    <div className="redd flex min-h-full w-full flex-col items-center justify-center">
+      <div
+        id="container"
+        className="redd flex w-full flex-col items-start justify-center gap-5 overflow-hidden px-[5%] laptop:px-[15%]"
+      >
+        <HeroSection />
+        <DemoSection />
+        {/* <MobileDemoSection /> */}
+        <GridLayout />
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/App/Global/NavPopover.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {} from '../../ui/po'
+const NavPopover = () => {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default NavPopover;
