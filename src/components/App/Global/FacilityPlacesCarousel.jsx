bac76345d456b760eef8bfcb61431dfcb1c3b4cd
egostixcorp <initcompany0@gmail.com>
2025-11-07 10:32:21 +0530

    update

--- src/components/App/Global/FacilityPlacesCarousel.jsx
+++ src/components/App/Global/FacilityPlacesCarousel.jsx
@@ -41,7 +41,7 @@ const FacilityPlacesCarousel = ({ cityName, facilities }) => {
       </div>
 
       {/* Carousel Section */}
-      <div className="redd w-fullf relative w-[19rem] laptop:max-w-4xl desktop:max-w-4xl">
+      <div className="redd w-fullf relative min-w-[20rem] laptop:max-w-4xl desktop:max-w-4xl">
         <Carousel
           opts={{
             align: "start",
